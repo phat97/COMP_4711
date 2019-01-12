bddd58a8e21fddc84ecf9dbdd67c929f475fdeee
phatl <phat.le@hotmail.com>
2019-01-11 21:25:52 -0800

    Updated server to add https

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+
+const options = {
+  cert: fs.readFileSync("../ssl_bundle/domain.cert.pem")
+};
+
 const app = express();
 
-const _PORT = process.env.PORT || 80;
+const HTTPS_PORT = 443;
+const HTTP_PORT = 8000;
 
-app.listen(_PORT, () => {
-    console.log(`Server is listening on port ${_PORT}`);
+https.createServer(options, app).listen(HTTPS_PORT, () => {
+  console.log(`Server is listening on port ${HTTPS_PORT}`);
 });
 
+http.createServer(app).listen(HTTP_PORT, () => {
+  console.log(`Server is listening on port ${HTTP_PORT}`);
+});
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-  });
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
